bbf4a3056950bd659decbf69ee36b8ebeebfb3b9
asunguyen <cntt510@gmail.com>
2023-11-28 06:10:07 +0700

    update

--- src/assets/streaming.js
+++ src/assets/streaming.js
@@ -20,7 +20,7 @@ socket.on("onData", (data) => {
         }
         const lastDailyBar = subscriptionItem ? subscriptionItem.lastDailyBar : null;
         var lastBar = lastDailyBar;
-        var lastBarTimestamp = Math.floor(lastBar.time/1000);
+        var lastBarTimestamp = Math.floor(lastBar.time / 1000);
         const isNewBar = JSON.stringify(lastBar) === '{}';
 
         let resolution = subscriptionItem.resolution;
@@ -39,7 +39,7 @@ socket.on("onData", (data) => {
                 ...lastDailyBar,
                 symbol: lastDailyBar.symbol,
                 resolution: subscriptionItem.resolution,
-                time: roundedTimestamp*1000,
+                time: roundedTimestamp * 1000,
                 open: newData.price,
                 high: isNewBar ? newData.price : lastBar.close,
                 low: isNewBar ? newData.price : lastBar.close,
@@ -47,7 +47,7 @@ socket.on("onData", (data) => {
                 volume: newData.volume
             };
         }
-        else{
+        else {
             upBar = {
                 ...lastDailyBar,
                 high: Math.max(lastDailyBar.high, bar.high),
@@ -63,7 +63,7 @@ socket.on("onData", (data) => {
         subscriptionItem.lastDailyBar = upBar;
         // Send data to every subscriber of that symbol
         subscriptionItem.handlers.forEach(handler => handler.callback(upBar));
-    }catch(err) {
+    } catch (err) {
         console.log(err);
     }
 })
@@ -103,7 +103,7 @@ export function subscribeOnStream(
         handlers: [handler],
     };
     channelToSubscription.set(symbolList, subscriptionItem);
-    socket.emit('changeSymbol', {symbolInfo: symbolInfo});
+    socket.emit('changeSymbol', { symbolInfo: symbolInfo });
 }
 
 export function unsubscribeFromStream(subscriberUID) {
@@ -138,14 +138,14 @@ function updateBar(newData, subscriber, lastDailyBar) {
     }
     const interval = resolution * 60;
     const roundedTimestamp = Math.floor(newData.ts / interval) * interval;
-    const lastBarTimestamp =  Math.floor(lastBar.time / 1000);
+    const lastBarTimestamp = Math.floor(lastBar.time / 1000);
 
     let updatedBar = false;
     if (isNewBar || roundedTimestamp > lastBarTimestamp) {
         updatedBar = {
             symbol: subscriber.symbol,
             resolution: subscriber.resolution,
-            time: roundedTimestamp*1000,
+            time: roundedTimestamp * 1000,
             open: newData.Open,
             high: isNewBar ? newData.Hight : lastBar.close,
             low: isNewBar ? newData.Low : lastBar.close,
@@ -170,27 +170,27 @@ function updateBar(newData, subscriber, lastDailyBar) {
 const socketdchart = io("https://dchart-socket.vndirect.com.vn/socket.io", {
     'transports': ["websocket", "polling"],
     'query': {
-      'symbol': "VN30F1M"
+        'symbol': "VN30F1M"
     }
-  });
+});
 
 socketdchart.on('connect', () => {
-	console.log('[socket] Connected');
+    console.log('[socket] Connected');
 });
 
 socketdchart.on('disconnect', (reason) => {
-	console.log('[socket] Disconnected:', reason);
+    console.log('[socket] Disconnected:', reason);
 });
 
 socketdchart.on('error', (error) => {
-	console.log('[socket] Error:', error);
+    console.log('[socket] Error:', error);
 });
 
 socketdchart.on('price', data => {
     let parsedData = data;
-	try {
+    try {
         parsedData = JSON.parse(data);
-    } catch (error) {}
+    } catch (error) { }
     const newData = {
         symbol: parsedData.symbol,
         ts: Math.floor(parsedData.time / 1000),
@@ -199,10 +199,10 @@ socketdchart.on('price', data => {
     };
 
     console.log('[socket] Message:', parsedData);
-	const symbolList = "CHART." + parsedData.symbol;
-	const subscriptionItem = channelToSubscription.get(symbolList);
+    const symbolList = "CHART." + parsedData.symbol;
+    const subscriptionItem = channelToSubscription.get(symbolList);
     const lastDailyBar = subscriptionItem.lastDailyBar;
-	var lastBar = lastDailyBar;
+    var lastBar = lastDailyBar;
     const isNewBar = JSON.stringify(lastBar) === '{}';
 
     let resolution = subscriptionItem.resolution;
@@ -214,10 +214,10 @@ socketdchart.on('price', data => {
 
     const interval = resolution * 60;
     const roundedTimestamp = Math.floor(newData.ts / interval) * interval;
-	if (subscriptionItem === undefined) {
-		return;
-	}
-    
+    if (subscriptionItem === undefined) {
+        return;
+    }
+
     console.log('[socket] newData:', newData);
     const bar = updateBar(newData, lastDailyBar, subscriptionItem);
     var upBar;
@@ -235,22 +235,162 @@ socketdchart.on('price', data => {
             volume: newData.volume
         };
     }
-    else{
+    else {
         upBar = {
             ...lastDailyBar,
             high: Math.max(lastDailyBar.high, bar.high),
             low: Math.min(lastDailyBar.low, bar.low),
             close: newData.price,
-            volume: lastDailyBar.low +newData.volume,
+            volume: lastDailyBar.low + newData.volume,
             time: bar.time,
         };
     }
-        
 
-    
-    
+
+
+
     subscriptionItem.lastDailyBar = upBar;
     //console.log('[socket] bar:', bar);
     // Send data to every subscriber of that symbol
     subscriptionItem.handlers.forEach(handler => handler.callback(upBar));
 })
+
+
+
+const socketUrl = "https://tradingviewrealtime.vps.com.vn";
+const subscribers = [];
+const socketSmart = io(socketUrl, {
+    transports: ["websocket", "polling"],
+    query: {
+        symbol: "VN30F1M"
+    }
+});
+
+socketSmart.on("connect", () => {
+    console.log("VPS Socket connected");
+});
+
+socketSmart.on("disconnect", (reason) => {
+    console.log("VPS Socket disconnected:", reason);
+});
+
+socketSmart.on("connect_error", (error) => {
+    console.log("VPS Socket connect error", error);
+});
+
+socketSmart.on("error", (error) => {
+    console.log("VPS Socket error", error);
+});
+
+socketSmart.on("stockps", function (data) {
+    let parsedData = data;
+    try {
+        parsedData = JSON.parse(data);
+    } catch (error) { }
+    console.log("parsedData:: ", parsedData);
+    if (parsedData.hasOwnProperty('t')) {
+        const newData = {
+            symbol: parsedData.s,
+            ts: parsedData.t,
+            volume: parseFloat(parsedData.lv),
+            price: parseFloat(parsedData.c)
+        };
+
+        var subscriber = subscribers.find(sub => sub.symbol === parsedData.s);
+        if (!subscriber && subscribers.length === 1) {
+            subscriber = subscribers[0];
+        }
+
+        if (subscriber) {
+            if (newData.ts < subscriber.lastBar.time / 1000) {
+                return;
+            }
+
+            subscriber.lastBar = updateBar1(newData, subscriber);
+            subscriber.lastBar.source = "VPS";
+
+            // self.postMessage({
+            //     type: "bar_updated",
+            //     data: subscriber.lastBar
+            // });
+        }
+    }
+});
+
+// self.onmessage = event => {
+//     if (event.data) {
+//         const eventData = event.data;
+//         if (eventData.type === "subscribe") {
+//             const newSubscriber = {
+//                 id: eventData.id,
+//                 symbol: eventData.symbol,
+//                 resolution: eventData.resolution,
+//                 lastBar: {}
+//             };
+
+//             subscribers.push(newSubscriber);
+//             const symbolList = "CHART." + eventData.symbol;
+//             const jsonString = JSON.stringify({
+//                 action: "join",
+//                 list: symbolList
+//             });
+
+//             socket.emit("regs", jsonString);
+//         } else if (eventData.type === "unsubscribe") {
+//             const index = subscribers.findIndex(sub => sub.id === eventData.id);
+//             if (index >= 0) {
+//                 subscribers.splice(index, 1);
+//             }
+
+//             const symbolList = "CHART." + eventData.symbol;
+//             const jsonString = JSON.stringify({
+//                 action: "leave",
+//                 list: symbolList
+//             });
+
+//             socket.emit("regs", jsonString);
+//         }
+//     }
+// };
+
+function updateBar1(newData, subscriber) {
+    const lastBar = subscriber.lastBar || {};
+    const isNewBar = JSON.stringify(lastBar) === '{}';
+
+    let resolution = subscriber.resolution;
+    if (resolution.includes('D')) {
+        resolution = 1440;
+    } else if (resolution.includes('W')) {
+        resolution = 10080;
+    }
+
+    const interval = resolution * 60;
+    const roundedTimestamp = Math.floor(newData.ts / interval) * interval;
+    const lastBarTimestamp = lastBar.time / 1000;
+
+    let updatedBar = false;
+    if (isNewBar || roundedTimestamp > lastBarTimestamp) {
+        updatedBar = {
+            symbol: subscriber.symbol,
+            resolution: subscriber.resolution,
+            time: roundedTimestamp * 1000,
+            open: newData.price,
+            high: isNewBar ? newData.price : lastBar.close,
+            low: isNewBar ? newData.price : lastBar.close,
+            close: newData.price,
+            volume: newData.volume
+        };
+    } else {
+        if (newData.price < lastBar.low) {
+            lastBar.low = newData.price;
+        } else if (newData.price > lastBar.high) {
+            lastBar.high = newData.price;
+        }
+
+        lastBar.volume += newData.volume;
+        lastBar.close = newData.price;
+        updatedBar = lastBar;
+    }
+
+    return updatedBar;
+}
